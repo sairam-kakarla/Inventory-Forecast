ecf18765368415e4d39f8a23905714fe42f830d9
sairam-kakarla <lakshmisairam.kakarla2019@vitstudent.ac.in>
2020-09-26 11:44:48 +0530

    model.py

--- model.py
+++ model.py
@@ -4,16 +4,54 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import confusion_matrix,accuracy_score
 import matplotlib.pyplot as plt
-dataset=pd.read_csv('S:/materials/fall2020/cse2003/project/2003proj/jan2020.csv')
-dataset['Date']=pd.to_datetime(dataset.Date)
-x=dataset[['Day','Festival','Holiday','Discount']]
-y=dataset['Quantity']
-x_train,x_test,y_train,y_test=train_test_split(x,y,train_size=0.2,random_state=2)
-regressor=LinearRegression()
-regressor.fit(x_train,y_train)
-LinearRegression(copy_X=True,fit_intercept=True,n_jobs=None,normalize=False)
+
+class model:
+    def load_data(self,location):
+        self.location=location
+        self.dataset=pd.read_csv(self.location)
+        self.dataset['Date']=pd.to_datetime(self.dataset.Date)
+        self.x=self.dataset[['Day','Festival','Holiday','Discount']]
+        self.y=self.dataset['Quantity']
+
+    def predict_model(self,data):
+        if data!=None:
+            self.x_test=pd.DataFrame(data)
+        self.predict_data=self.regressor.predict(self.x_test)
+        self.data_frame=pd.DataFrame({'Actual Price':self.y_test,"Predicted Price":self.predict_data})
+        print(self.data_frame)
+        
+            
+
+
+
+    def train_model(self,split):
+        self.regressor=LinearRegression()
+        if split!=0:
+            self.x_train,self.x_test,self.y_train,self.y_test=train_test_split(self.x,self.y,train_size=split,random_state=2)
+        else:
+            self.x_train=self.x
+            self.y_train=self.y
+        self.regressor.fit(self.x_train,self.y_train)
+        LinearRegression(copy_X=True,fit_intercept=True,n_jobs=None,normalize=True)
+
+if __name__=='__main__':
+    model1=model()
+    model1.load_data("S:/materials/fall2020/cse2003/project/2003proj/jan2020.csv")
+    model1.train_model(0.9)
+    model1.predict_model(None)
+
+
+
+
+
+
+
+
+
+
+
+'''
 predict=regressor.predict(x_test)
 df=pd.DataFrame({'Actual Quantity':y_test,'Predicted Quantity':predict})
 print(regressor.score(x_test,y_test))
-df.plot(kind='bar')
-plt.show()
+'''
