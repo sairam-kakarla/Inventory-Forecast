82cb0a0d689abf8ffe9977f04f77d6716dbb7c2c
sairam-kakarla <lakshmisairam.kakarla2019@vitstudent.ac.in>
2020-09-27 07:40:35 +0530

    greedy of p/s

--- storage.py
+++ storage.py
@@ -27,17 +27,37 @@ def sort_rack(rack):
             if rack[min][1] >rack[j][1]:
                 min=j
         rack[min],rack[i]=rack[i],rack[min]
-
+# Greedy about profit percentage by stock approach to find 
+# optimal cost price  and for maximum profits and optimal stock
 class Rack_Maxim:
     
     @staticmethod
     def getMaxValue(rack,capacity):
         max_storage=capacity
+        max_profit=0
         total_CP=0
         sort_rack(rack)
         index=len(rack)-1
-        while(capacity>0):
-            pass
+        log={}
+        while(max_storage>0):
+            if rack[index][2]<=max_storage:
+                log[rack[index][0]]=(rack[index][2],rack[index][4])
+                total_CP+=rack[index][2]*rack[index][4]
+                max_profit+=rack[index][2]*rack[index][3]
+                max_storage-=rack[index][2]
+            elif rack[index][2]>max_storage:
+                log[rack[index][0]]=(max_storage,rack[index][4])
+                total_CP+=max_storage*rack[index][4]
+                max_profit+=max_storage*rack[index][3]
+                max_storage-=max_storage
+            index-=1
+        print("Available stock capacity: ",capacity)
+        for i in log.keys():
+            print(i," stock: ",log[i][0]," cost price: ",log[i][1])
+        print("Total cost price: ",total_CP)
+        print("Maximum profit estimated: ",max_profit)
+
+            
 
 
             
@@ -47,7 +67,9 @@ class Rack_Maxim:
 
 
 
-
+'''Some random test cases
+Need to create a proper interface for input and connection with models.py for
+stock predictions.'''
 if __name__=='__main__':
     rice_rack=space("rice",100)
     rice1=item("rice","Pusa",4500,4200,5,25)
@@ -58,11 +80,8 @@ if __name__=='__main__':
     rack=[rice1,rice2,rice3,rice4,rice5]
     rack_main=[]
     for i in rack:
-        rack_main.append((i.brand,round(i.profit_percent()/i.stock,6),i.stock,i.profit(),i.CP))
-    sort_rack(rack_main)
-    for i in rack_main:
-        print(i)
-    
+        rack_main.append((i.brand,round((i.profit_percent()/i.stock),8),i.stock,i.profit(),i.CP))
+    Rack_Maxim.getMaxValue(rack_main,rice_rack.capacity)
     
 
 
